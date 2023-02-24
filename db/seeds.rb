8b6e2874ca0e4e177a06dcac0084097fbaf23183
Patrick Santos <patrick.sg.santos@gmail.com>
2023-02-24 16:41:48 -0300

    a basic airbnb made with rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+30.times do
+  flat = Flat.new(
+    name: Faker::Address.community,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence(word_count: 60, supplemental: false, random_words_to_add: 4),
+    price_per_night: (50..300).to_a.sample,
+    number_of_guest: (2..8).to_a.sample)
+  flat.save
+end
